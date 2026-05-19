2a2e56f6a1332c1fd9669bdd5f6d116b9fe890ba
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-05-19 00:59:05 -0500

    Fix: output combinations to file

--- cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hashcat -a 1 --stdout "$1" "$2" >/dev/null 2>&1; while read a; do while read b; do echo "$a$b"; done < "$2"; done < "$1" > combined.txt
+hashcat -a 1 --stdout "$1" "$2" >/dev/null 2>&1; while read a; do while read b; do echo "$a$b"; done < "$2"; done < "$1" > 8-combined.txt
