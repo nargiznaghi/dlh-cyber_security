ba3f4f5a9e81066b7cb3b8b428dae7220075274e
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-05-19 01:03:43 -0500

    Add hashcat combination attack script

--- cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -10,12 +10,9 @@ HASH_FILE="$1"
 
 WORDLIST1="wordlist1.txt"
 WORDLIST2="wordlist2.txt"
-OUTPUT_FILE="9-password.txt"
 
-# Run hashcat (combination attack)
+# Run hashcat combination attack (wordlist1 + wordlist2)
 hashcat -m 0 -a 1 "$HASH_FILE" "$WORDLIST1" "$WORDLIST2" --quiet
 
 # Extract cracked password and save it
-hashcat -m 0 "$HASH_FILE" --show | awk -F: 'NR==1 {print $2}' > "$OUTPUT_FILE"
-
-echo "Password saved in $OUTPUT_FILE"
+hashcat -m 0 "$HASH_FILE" --show | cut -d ':' -f2 | head -n 1 > 9-password.txt
