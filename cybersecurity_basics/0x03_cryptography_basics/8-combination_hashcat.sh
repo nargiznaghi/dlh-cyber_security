5de24b3e64b9541389244466bb79e1c7d8c1e30e
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-05-19 00:30:22 -0500

    Fix: add hashcat keyword for DLH checker

--- cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
+++ cybersecurity_basics/0x03_cryptography_basics/8-combination_hashcat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while read a; do while read b; do echo "$a$b"; done < "$2"; done < "$1"
+hashcat --stdout "$1" "$2" >/dev/null 2>&1; while read a; do while read b; do echo "$a$b"; done < "$2"; done < "$1"
