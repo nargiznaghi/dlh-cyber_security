b2277f3b09ee057a37a2d0f7deedc2527b3788dd
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-05-20 16:43:30 -0500

    Save local changes before rebase

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1 && john --show --format=Raw-MD5 "$1" | grep ':' | cut -d ':' -f2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null 2>&1; john --show "$1" | cut -d ':' -f2 > 4-password.txt
