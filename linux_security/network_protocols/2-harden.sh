4007edf10dbcf7295f7bc9c7eb9c35ab9206f251
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-05-26 06:26:44 -0500

    Add 2-line hardening script

--- linux_security/network_protocols/2-harden.sh
+++ linux_security/network_protocols/2-harden.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Find all world-writable directories and secure them
-
-find / -type d -perm -0002 2>/dev/null | while read dir; do
-  echo "$dir"
-  chmod o-w "$dir"
-done
-
-echo "done"
+find / -type d -perm -0002 -exec chmod o-w {} \;
